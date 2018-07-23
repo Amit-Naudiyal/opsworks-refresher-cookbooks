147c5c272c054361939fa9afa03d72a151536bc9
Naudiyal <anaudiya@9801a7ab9fad.ant.amazon.com>
2018-07-23 21:41:27 +1000

    changes

--- nodeobject-chefsearch/recipes/default.rb
+++ nodeobject-chefsearch/recipes/default.rb
@@ -1,6 +1,6 @@
 
 
-Chef::Log.info("********** The app's URL is '#{node['deploy']['php-training']['scm']['repository']}' **********")
+Chef::Log.info("********** The app's URL is '#{node['deploy']['php_training']['scm']['repository']}' **********")
 
 
 dbserver = search(:node, "layers:php-app").first
