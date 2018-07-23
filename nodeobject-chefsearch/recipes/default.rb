2c86fcbabe67f17f449c9222368f2d09cd08d508
Naudiyal <anaudiya@9801a7ab9fad.ant.amazon.com>
2018-07-23 22:59:18 +1000

    changes

--- nodeobject-chefsearch/recipes/default.rb
+++ nodeobject-chefsearch/recipes/default.rb
@@ -2,6 +2,7 @@
 
 Chef::Log.info("********** The app's URL is '#{node['deploy']['php_training']['scm']['repository']}' **********")
 
+Chef::Log.info("********** The PrivateIP taken from NodeObject is '#{node['opsworks']['layers']['my-custome-layer']['instances']['refresherday4nodeobjects']['private_ip']}' **********")
 
-dbserver = search(:node, "layers:my-custome-layer").first
-Chef::Log.info("Private IP: '#{refresherday4nodeobjects[:private_ip]}'")
+myserver = search(:node, "layers:my-custome-layer").first
+Chef::Log.info("**********Private IP taken by ChefSearch: '#{myserver[:private_ip]}'**********")
