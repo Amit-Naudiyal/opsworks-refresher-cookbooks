e9aedd89d58ddcac7210bdd1344e818953cdf25a
Naudiyal <anaudiya@9801a7ab9fad.ant.amazon.com>
2018-07-29 22:49:18 +1000

    databag

--- nodeobject-chefsearch/recipes/databags.rb
+++ nodeobject-chefsearch/recipes/databags.rb
@@ -6,9 +6,9 @@ Chef::Log.info("**** The Instance ID is: '#{myserver['ec2_instance_id']}' ****")
 
 
 
-myserver = search("aws_opsworks_instance").third
+myserver = search("aws_opsworks_instance").last
 Chef::Log.info("**** The private IP address is: '#{myserver['private_ip']}' ****")
 
-myserver = search("aws_opsworks_instance").third
+myserver = search("aws_opsworks_instance").last
 Chef::Log.info("**** The Instance ID is: '#{myserver['ec2_instance_id']}' ****")
 
